a9951ffef50b44139b7fce85a4a69b53abc7a438
JerichoBelino <jerichobelino15@gmail.com>
2024-12-02 00:26:52 +0800

    Add Function for User Login

--- class.php
+++ class.php
@@ -29,6 +29,26 @@
 		}
 
 
+        // User Login
+        public function user_login($username, $password) {
+			// Prepare the query with placeholders
+			$query = "SELECT * FROM `sign_up` WHERE `username` = :username";
+			$stmt = $this->conn->prepare($query);
+			$stmt->bindParam(':username', $username, PDO::PARAM_STR);
 		
+			if ($stmt->execute()) {
+				$fetch = $stmt->fetch(PDO::FETCH_ASSOC);
+				$valid = $fetch ? 1 : 0; // 1 if user found, 0 if not
+				if ($valid && password_verify($password, $fetch['password'])) {
+					return array(
+						'id' => isset($fetch['id']) ? $fetch['id'] : 0,
+						'count' => $valid
+					);
+				}
+			}
+			return array('user_id' => 0, 'count' => 0);
+		}
+		
+        
 	}
 ?>
