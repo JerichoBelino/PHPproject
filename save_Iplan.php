3c5892053477e2177ffd84058f29dc32c41036c1
chryzel <chryzeldetorres@gmail.com>
2024-12-04 21:32:28 +0800

    save_Iplan

---
+++ save_Iplan.php
@@ -0,0 +1,12 @@
+<?php
+	require_once 'class.php';
+	if(ISSET($_POST['save'])){
+		$db=new db_class();
+		$lplan_month=$_POST['lplan_month'];
+		$lplan_interest=$_POST['lplan_interest'];
+		$lplan_penalty=$_POST['lplan_penalty'];
+		
+		$db->save_lplan($lplan_month,$lplan_interest,$lplan_penalty);
+		header("location: admin_loan_plan.php");
+	}
+?>
