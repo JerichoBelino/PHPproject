629e061430a99ae15b386d6f01f395090021b8b5
JerichoBelino <jerichobelino15@gmail.com>
2024-12-06 01:24:46 +0800

    Add the crud for Borrowers/Account

--- class.php
+++ class.php
@@ -1,11 +1,17 @@
 <?php
-	require_once 'config.php';
 	
-	class db_class extends db_connect{
-		
-		public function __construct(){
-			$this->connect();
+	class db_class {
+
+		private $conn;
+
+		public function __construct($db){
+			$this->conn = $db;
 		}
+
+		 // To access the connection and to interact with conn
+		public function getConnection(){
+			 return $this->conn;
+		 }
 		
 		// Admin Login
 		public function login($username, $password){
@@ -28,7 +34,6 @@
 			return array('user_id' => 0, 'count' => 0);
 		}
 
-
         // User Login
         public function user_login($username, $password) {
 			// Prepare the query with placeholders
@@ -48,6 +53,7 @@
 			}
 			return array('user_id' => 0, 'count' => 0);
 		}
+
 		// Sign up
 		public function sign_up($username, $password) {
 			// Prepare the SQL statement for inserting a new user into the 'sign_up' table
@@ -66,7 +72,6 @@
 			}
 		}
 		
-
 		// Username is taken, passed by reference
 		public function if_username_taken($username, &$count) {
 			// Prepare the SQL query to check if the username already exists
@@ -81,6 +86,69 @@
 			// Return true if the count is greater than 0 (meaning the username is taken)
 			return $count > 0;
 		}
+
+
+		// Function for Borrower
+		public function save_borrower($firstname, $middlename, $lastname, $contact_no, $address, $email) {
+			try {
+				$query = "INSERT INTO `borrower` (`firstname`, `middlename`, `lastname`, `contact_no`, `address`, `email`) VALUES (:firstname, :middlename, :lastname, :contact_no, :address, :email)";
+				$stmt = $this->conn->prepare($query);
+				// Bind parameters to the query
+				$stmt->bindParam(':firstname', $firstname, PDO::PARAM_STR);
+				$stmt->bindParam(':middlename', $middlename, PDO::PARAM_STR);
+				$stmt->bindParam(':lastname', $lastname, PDO::PARAM_STR);
+				$stmt->bindParam(':contact_no', $contact_no, PDO::PARAM_STR);
+				$stmt->bindParam(':address', $address, PDO::PARAM_STR);
+				$stmt->bindParam(':email', $email, PDO::PARAM_STR);
+				if ($stmt->execute()) {
+					return true;
+				}
+				return false;
+			} catch (Exception $e) {
+				throw new Exception("Error saving borrower: " . $e->getMessage());
+			}			
+		}
+        // Display
+		public function display_borrower() {
+			// Prepare the query
+			$query = "SELECT * FROM borrower" ;
+			$stmt = $this->conn->prepare($query);
+			if ($stmt->execute()) {
+				// Fetch all results
+				$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+				return $result;
+			}
+			return false;
+		}
+        // Delete
+		public function delete_borrower($borrower_id) {
+			$query = "DELETE FROM `borrower` WHERE `borrower_id` = :borrower_id";
+			$stmt = $this->conn->prepare($query);
+			// Bind the borrower_id parameter
+			$stmt->bindParam(':borrower_id', $borrower_id, PDO::PARAM_INT);
+			if ($stmt->execute()) {
+				return true;
+			}
+			return false;
+		}
+        // Update
+		public function update_borrower($borrower_id, $firstname, $middlename, $lastname, $contact_no, $address, $email) {
+			$query = "UPDATE `borrower` SET `firstname` = :firstname, `middlename` = :middlename, `lastname` = :lastname, `contact_no` = :contact_no, `address` = :address, `email` = :email WHERE `borrower_id` = :borrower_id";
+			$stmt = $this->conn->prepare($query);
+			// Bind the parameters to the query
+			$stmt->bindParam(':firstname', $firstname, PDO::PARAM_STR);
+			$stmt->bindParam(':middlename', $middlename, PDO::PARAM_STR);
+			$stmt->bindParam(':lastname', $lastname, PDO::PARAM_STR);
+			$stmt->bindParam(':contact_no', $contact_no, PDO::PARAM_STR);
+			$stmt->bindParam(':address', $address, PDO::PARAM_STR);
+			$stmt->bindParam(':email', $email, PDO::PARAM_STR);
+			$stmt->bindParam(':borrower_id', $borrower_id, PDO::PARAM_INT);
+			if ($stmt->execute()) {
+				return true;
+			}
+			return false;
+		}
+
         
 	}
 ?>
