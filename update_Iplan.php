c11b68406e114740d61899a43ba7623ee5e52437
chryzel <chryzeldetorres@gmail.com>
2024-12-06 03:14:27 +0800

    updated update_Iplan

--- update_Iplan.php
+++ update_Iplan.php
@@ -1,12 +1,19 @@
 <?php
-	require_once'class.php';
+	require_once 'class.php';
+	require_once 'config.php';
+	session_start();
+	
 	if(ISSET($_POST['update'])){
-		$db=new db_class();
+
+		$database = new db_connect();
+		$db = $database->connect();
+		$admin = new db_class($db);
+
 		$lplan_id=$_POST['lplan_id'];
 		$lplan_month=$_POST['lplan_month'];
 		$lplan_interest=$_POST['lplan_interest'];
 		$lplan_penalty=$_POST['lplan_penalty'];
-		$db->update_lplan($lplan_id,$lplan_month,$lplan_interest,$lplan_penalty);
+		$admin->update_lplan($lplan_id,$lplan_month,$lplan_interest,$lplan_penalty);
 		echo"<script>alert('Update loan Plan successfully!')</script>";
 		echo"<script>window.location='admin_loan_plan.php'</script>";
 	}
