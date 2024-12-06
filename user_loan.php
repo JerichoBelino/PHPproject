7c604aa0653d40eb5fd612806c1b54472081ca6b
JerichoBelino <jerichobelino15@gmail.com>
2024-12-06 14:56:31 +0800

    Add a sidebar and logout

--- user_loan.php
+++ user_loan.php
@@ -31,3 +31,43 @@
   	<link href="css/sb-admin-2.css" rel="stylesheet">
 
 </head>
+
+<body id="page-top">
+    <div id="wrapper">
+
+		<nav class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+                <div class="sidebar-brand-text mx-3">LOANS</div>
+            </a>
+			<li class="nav-item"><a class="nav-link" href="user_home.php"><i class="fas fa-fw fa-home"></i> Home</a></li>
+			<li class="nav-item"><a class="nav-link" href="user_loan.php"><i class="fas fa-fw fas fa-comment-dollar"></i> Loans</a></li>
+			<li class="nav-item"><a class="nav-link" href="user_payment.php"><i class="fas fa-fw fas fa-coins"></i> Payments</a></li>
+			<li class="nav-item"><a class="nav-link" href="user_borrower.php"><i class="fas fa-fw fas fa-book"></i> Borrowers</a></li>
+		</nav>
+ 
+        <div id="content-wrapper" class="d-flex flex-column">
+            <div id="content">
+                
+				<nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+                    <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+                        <i class="fa fa-bars"></i>
+                    </button>
+					<!-- Log out -->
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item dropdown no-arrow">
+                            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
+                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <span class="mr-2 d-none d-lg-inline text-gray-600 small"></span>
+                                <img class="img-profile rounded-circle"
+                                    src="image/admin_logo.png">
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
+                                aria-labelledby="userDropdown">
+                                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+                                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                                    Logout
+                                </a>
+                            </div>
+                        </li>
+                    </ul>
+                </nav>
